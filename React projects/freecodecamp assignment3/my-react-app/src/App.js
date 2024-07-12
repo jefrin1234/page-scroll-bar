b0a75026b72699e0abdb04c2d3b7f04ca39ff156
jefrin james <jamesjefrin13@gmail.com>
2024-07-12 09:19:16 +0530

    added al files

---
+++ React projects/freecodecamp assignment3/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css'
+import StarRating from './star-rating'
+
+function App() {
+  return (
+    <div className='App'>
+      <StarRating noOfStars={10}/>
+    </div>
+  )
+}
+
+export default App
---
+++ React projects/freecodecamp assignment7/my-react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import TreeView from './tree-view';
+import menus from './tree-view/data';
+
+function App() {
+ 
+  return (
+    <div className="App">
+
+      <TreeView menus={menus} />
+
+     
+    </div>
+  );
+}
+
+export default App;
+
